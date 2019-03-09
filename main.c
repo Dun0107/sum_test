72ed2fd9830d4f426e64da344d8121491ce00838
Dun0107 <40848202+Dun0107@users.noreply.github.com>
2019-03-09 22:00:33 +0900

    Add sum*

---
+++ main.c
@@ -0,0 +1,15 @@
+#include "sum.h"
+
+int main(void)
+{
+	int a;
+	int b;
+
+	printf("정수 두개를 입력하시오: ");
+	scanf_s("%d %d", &a, &b);
+
+	int result = sum(a, b);
+	printf("%d에서 %d까지의 총합은 %d입니다", a, b, result);
+	return 0;
+
+}
---
+++ sum.c
@@ -0,0 +1,4 @@
+int sum(int a, int b)
+{
+	return(a + b)*(abs(a - b) + 1) / 2;
+}
